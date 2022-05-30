fd7f954c6c941e0b122924d0d4bacbe1ceb85925
Obaid Ullah <obullah.95@gmail.com>
2022-05-30 12:41:44 +0530

    Day 1 Commit

---
+++ src/test/java/com/brillio/test/LoginTest.java
@@ -0,0 +1,13 @@
+package com.brillio.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void validCredentialTest()
+	{
+		System.out.println("Valid");
+	}
+
+}

e3e77382aec8ab7b920f941d149385d5a2e34cd2
Obaid Ullah <obullah.95@gmail.com>
2022-05-31 10:25:08 +0530

    added before and after method

--- src/test/java/com/brillio/test/LoginTest.java
+++ src/test/java/com/brillio/test/LoginTest.java
@@ -6,11 +6,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
+	
+	@BeforeMethod
+	public void setup()
+	{
+		System.out.println("Before Method");
+	}
+	
+	@AfterMethod
+	public void setup1()
+	{
+		System.out.println("After Method");
+	}
 	
 	@Test
 	public void validCredentialTest()

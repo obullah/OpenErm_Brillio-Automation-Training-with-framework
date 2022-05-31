25e3c6a25e6d80d7e57fb44d4947800f9f587353
Obaid Ullah <obullah.95@gmail.com>
2022-05-31 09:58:00 +0530

    commit day2

--- src/test/java/com/brillio/test/LoginTest.java
+++ src/test/java/com/brillio/test/LoginTest.java
@@ -1,6 +1,14 @@
 package com.brillio.test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
 	
@@ -8,6 +16,36 @@ public class LoginTest {
 	public void validCredentialTest()
 	{
 		System.out.println("Valid");
+		//System.setProperty("webdriver.chrome.driver", "E:\\Softwares\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demo.openemr.io/b/openemr");
+		driver.findElement(By.id("authUser")).sendKeys("admin");
+		driver.findElement(By.id("clearPass")).sendKeys("pass");
+		driver.findElement(By.cssSelector("#login-button")).click();
+		
+		String actualTitle= driver.getTitle();
+		Assert.assertEquals(actualTitle, "OpenEMR");
+	}
+	
+	@Test
+	public void invalidCredentialTest()
+	{
+		System.out.println("Valid");
+		//System.setProperty("webdriver.chrome.driver", "E:\\Softwares\\chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demo.openemr.io/b/openemr");
+		driver.findElement(By.id("authUser")).sendKeys("john");
+		driver.findElement(By.id("clearPass")).sendKeys("1234");
+		driver.findElement(By.cssSelector("#login-button")).click();
+		
+		String actualError = driver.findElement(By.xpath("//div[contains(text(),'Invalid']")).getText();
+		Assert.assertEquals(actualError, "Invalid username or password");
 	}
 
 }
